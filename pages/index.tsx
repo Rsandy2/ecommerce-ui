028a7ec4369da8486c9b1e4d43a032afa53f0c49
Raejae Sandy <rsandy@Raejaes-MacBook-Air.local>
2022-03-23 22:46:07 -0400

    feat(be): Updated Prisma Routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,17 @@
 import type { NextPage } from "next";
 import styles from "../styles/Home.module.scss";
+import toast, { Toaster } from "react-hot-toast";
 
+const notify = () => toast("Here is a toast.");
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <button className={styles.button} onClick={notify}>
+        TOASTER
+      </button>
+      <Toaster />
+    </div>
+  );
 };
 
 export default Home;
