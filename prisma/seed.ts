9a08aa2d9589b2d848b130740951e61c6a4e49fb
Raejae Sandy <Raesandy2@gmail.com>
2022-03-23 23:32:45 -0400

    Rsandy/add prisma routes (#2)
    * feat: Set default template and configs
    
    * feat(be): Updated Prisma Routes
    
    Co-authored-by: Raejae Sandy <rsandy@s172-20-190-h197.paws.uga.edu>
    Co-authored-by: Raejae Sandy <rsandy@Raejaes-MacBook-Air.local>

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,3 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@gmail.com",
+    },
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => await prisma.$disconnect);
