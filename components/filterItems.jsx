4d4f8b02bf3273aa5230095ea312b0898f09bcf3
Oscar Parada <op66954@uga.edu>
2022-04-13 22:16:38 -0400

    (feat): Added profile page
    Need to add form functionality

---
+++ components/filterItems.jsx
@@ -0,0 +1,208 @@
+function FilterItems() {
+  return (
+    <div>
+      <h3>Filters</h3>
+      <nav className="nav nav-pills flex-column">
+        <hr></hr>
+        <h6>Genre</h6>
+        <form>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Action and Adventure
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckChecked"
+              checked
+            />
+            <label className="form-check-label" for="flexCheckChecked">
+              Classics
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Comic and Graphic Novels
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Detective
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Fantasy
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Historical Fiction
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Horror
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Science Fiction
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Thriller and Suspense
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Romance
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Contemporary
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Young Adult
+            </label>
+          </div>
+        </form>
+        <hr></hr>
+        <h6>Vender</h6>
+        <form>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Sample Vender
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckChecked"
+              checked
+            />
+            <label className="form-check-label" for="flexCheckChecked">
+              Sample Vender
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Sample Vender
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Sample Vender
+            </label>
+          </div>
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value=""
+              id="flexCheckDefault"
+            />
+            <label className="form-check-label" for="flexCheckDefault">
+              Sample Vender
+            </label>
+          </div>
+        </form>
+      </nav>
+    </div>
+  );
+}
+
+export default FilterItems;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,36 +9,32 @@ const Home: NextPage = () => {
   const data = {
     bookTitle: "The Lightning Thief",
     authorName: "Rick Riordan",
-    bookCoverUrl: "https://images-na.ssl-images-amazon.com/images/I/91RQ5d-eIqL.jpg",
+    bookCoverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/91RQ5d-eIqL.jpg",
     altText: "Percy Jackson Book Cover",
     buttonText: "Add to cart",
-    bookPrice: "$15.00"
+    bookPrice: "$15.00",
   };
 
   const data2 = {
     bookTitle: "City of Bones",
     authorName: "Cassandra Clare",
-    bookCoverUrl: "https://images-na.ssl-images-amazon.com/images/I/81GvNdZqETL.jpg",
+    bookCoverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/81GvNdZqETL.jpg",
     altText: "City of Bones Book Cover",
     buttonText: "Add to cart",
-    bookPrice: "$15.00"
+    bookPrice: "$15.00",
   };
 
- 
   return (
     <div className={styles.container}>
       <NavBar />
-      {/* <button className={styles.button} onClick={notify}>
-        TOASTER
-      </button>
-      <Toaster /> */}
       <div className={styles.card__container}>
-          <ProductCard productData={data} /> 
-          <ProductCard productData={data2} /> 
-          <ProductCard productData={data} /> 
-          <ProductCard productData={data2} />
+        <ProductCard productData={data} />
+        <ProductCard productData={data2} />
+        <ProductCard productData={data} />
+        <ProductCard productData={data2} />
       </div>
-
     </div>
   );
 };
