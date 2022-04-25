8dedc9d49addc51c818fbfbdb8d1ea370c566ddf
Raejae Sandy <Raesandy2@gmail.com>
2022-04-25 01:20:05 -0400

    admin Changes

---
+++ components/AddBook.tsx
@@ -0,0 +1,155 @@
+import React, { useState, Fragment } from "react";
+import { BsPlus, BsDash } from "react-icons/bs";
+import { BsCart3, BsArrowLeft } from "react-icons/bs";
+import { Dialog, Transition } from "@headlessui/react";
+//import styles from "../styles/AdminManage.module.scss";
+//import * as React from "react";
+import Link from "next/link";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import styles from "../styles/Login.module.scss";
+
+
+type ModalProp = {
+    addisOpen: boolean;
+    addsetIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const AddBook = ({ addisOpen, addsetIsOpen }: ModalProp) => {
+    // const [cart, setCart] = useState<ShoppingCart[]>();
+
+    return (
+        <>
+            <div>
+                <Transition appear show={addisOpen} as={Fragment}>
+                    <Dialog
+                        as="div"
+                        className="fixed inset-0 z-10 overflow-y-auto flex justify-center items-center"
+                        onClose={addsetIsOpen}
+                    >
+                        <div className="text-center flex flex-col min-w-full px-10">
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0"
+                                enterTo="opacity-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100"
+                                leaveTo="opacity-0"
+                            >
+                                <Dialog.Overlay className="fixed inset-0 bg-black bg-opacity-50" />
+                            </Transition.Child>
+                            <span className="" aria-hidden="true">
+                                &#8203;
+                            </span>
+
+                            <Transition.Child
+                                as={Fragment}
+                                enter="ease-out duration-300"
+                                enterFrom="opacity-0 scale-95"
+                                enterTo="opacity-100 scale-100"
+                                leave="ease-in duration-200"
+                                leaveFrom="opacity-100 scale-100"
+                                leaveTo="opacity-0 scale-95"
+                            >
+                                {/* cart */}
+                               
+                                <div className="container" style={{ maxWidth: "50%" }}>
+                                    <div className="card">
+                                        <div className={styles.form_container}>
+                                            <h1 className={styles.form_title}>Add</h1>
+
+                                            <form> {/*onSubmit={registerUser}>*/}
+                                                <div className="row">
+                                                    <div className="col-lg-6">
+                                                        <div className={styles.form_content}>
+                                                            <input
+                                                                type="text"
+                                                                //value={email}
+
+                                                                required="required"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                            <label>Title</label>
+                                                            <div className={styles.line}></div>
+                                                        </div>
+
+                                                        <div className={styles.form_content}>
+                                                            <input
+                                                                type="text"
+                                                                //value={email}
+
+                                                                required="required"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                            <label>Author</label>
+                                                            <div className={styles.line}></div>
+                                                        </div>
+
+                                                        <div className={styles.form_content}>
+                                                            <input
+                                                                type="textarea"
+                                                                //value={email}
+                                                                required="required"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                            <label>Description</label>
+                                                            <div className={styles.line}></div>
+                                                        </div>
+
+                                                        <button onClick={() => addsetIsOpen(false)}>Back</button>
+                                                    </div>
+                                                    <div className="col-lg-6">
+                                                        <div className={styles.form_content}>
+                                                            <input
+                                                                type="text"
+                                                                //value={email}
+                                                                required="required"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                            <label>Vendor</label>
+                                                            <div className={styles.line}></div>
+                                                        </div>
+
+                                                        <div className={styles.form_content}>
+                                                            <input
+                                                                type="text"
+                                                                //value={email}
+                                                                required="required"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                            <label>Genre</label>
+                                                            <div className={styles.line}></div>
+                                                        </div>
+
+
+
+                                                        <div className={styles.form_content}>
+                                                            <input
+                                                                type="text"
+                                                                //value={email}
+                                                                required="required"
+                                                                onChange={(e) => setEmail(e.target.value)}
+                                                            />
+                                                            <label>Price</label>
+                                                            <div className={styles.line}></div>
+                                                        </div>
+                                                        <button type="submit">Update</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* end of cart  */}
+                            </Transition.Child>
+                        </div>
+                    </Dialog>
+                </Transition>
+            </div>
+        </>
+    );
+};
+export default AddBook;
---
+++ pages/adminOrders.jsx
@@ -0,0 +1,179 @@
+import { useState } from "react"; // Using to handle forms?
+import React from "react";
+import styles from "../styles/AdminManage.module.scss";
+import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import EditOrder from "../components/EditOrder";
+import AddOrder from "../components/AddOrder";
+// https://www.tutorialrepublic.com/codelab.php?topic=bootstrap&file=crud-data-table-for-database-with-modal-form
+
+export default function AdminPanel() {
+  const [inputs, setInputs] = useState({});
+  //const [isOpen, setIsOpen] = React.useState(false);
+  let [isOpen, setIsOpen] = useState(false);
+  let [addisOpen, addsetIsOpen] = useState(false);
+
+  return (
+    <div
+      className="h-screen w-full flex justify-center items-center"
+      style={{ backgroundColor: "#DDBEA9" }}
+    >
+      <div className="container-xl">
+        <div className="table_responsive" style={{ marginTop: "10%" }}>
+          <div className={styles.table_wrapper}>
+            <div className={styles.table_title}>
+              <div className="row">
+                <div className="col-sm-6">
+                  <h2>
+                    <b>Manage Orders</b>
+                  </h2>
+                </div>
+                <div className="col-sm-6">
+                  <a
+                    className="btn btn-success"
+                    data-toggle="modal"
+                    style={{ ...{ float: "right" }, ...{ marginLeft: "5%" } }}
+                    onClick={() => addsetIsOpen(!isOpen)}
+                  >
+                    <AddOrder
+                      addisOpen={addisOpen}
+                      addsetIsOpen={addsetIsOpen}
+                    />
+                    <i className="material-icons">&#xE147;</i>{" "}
+                    <span>Add New Order</span>
+                  </a>
+                  <a
+                    href="#deleteEmployeeModal"
+                    className="btn btn-danger"
+                    data-toggle="modal"
+                    style={{ float: "right" }}
+                  >
+                    <i className="material-icons">&#xE15C;</i>{" "}
+                    <span>checkbox check delete [opt ingnore]</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <table className="table table_striped table_hover">
+              <thead>
+                <tr>
+                  <th>Order ID?</th>
+                  <th>Name</th>
+                  <th>User ID</th>
+                  <th>Email</th>
+                  <th>Total Price</th>
+                  <th>Date</th>
+
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>123</td>
+                  <td>Clara Zhang</td>
+                  <td>420</td>
+                  <td>clarasemail@yahoo.com</td>
+                  <td>$99.99</td>
+                  <td>04/24/2022</td>
+
+                  <td>
+                    <a
+                      className="edit"
+                      data-toggle="modal"
+                      style={{ float: "left" }}
+                      onClick={() => setIsOpen(!isOpen)}
+                    >
+                      <EditOrder isOpen={isOpen} setIsOpen={setIsOpen} />
+                      <i data-toggle="tooltip" title="Edit">
+                        <FaEdit />
+                      </i>
+                    </a>
+                    <a
+                      className="delete"
+                      data-toggle="modal"
+                      style={{ float: "right" }}
+                    >
+                      <i data-toggle="tooltip" title="Delete">
+                        <FaTrash />
+                      </i>
+                    </a>
+                  </td>
+                </tr>
+                <tr>
+                  <td>002</td>
+                  <td>Bobby</td>
+                  <td>232</td>
+                  <td>cexample@gmail.com</td>
+                  <td>$10.00</td>
+                  <td>04/24/2022</td>
+
+                  <td>
+                    <a
+                      className="edit"
+                      data-toggle="modal"
+                      style={{ float: "left" }}
+                      onClick={() => setIsOpen(!isOpen)}
+                    >
+                      <i data-toggle="tooltip" title="Edit">
+                        <FaEdit />
+                      </i>
+                    </a>
+                    <a
+                      className="delete"
+                      data-toggle="modal"
+                      style={{ float: "right" }}
+                    >
+                      <i data-toggle="tooltip" title="Delete">
+                        <FaTrash />
+                      </i>
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div className="clearfix">
+              <div className="hint_text">
+                Showing <b>5</b> out of <b>25</b> entries
+              </div>
+              <ul className={styles.pagination}>
+                <li className="page-item disabled">
+                  <a href="#">Previous</a>
+                </li>
+                <li className="page-item">
+                  <a href="#" className="page-link">
+                    1
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a href="#" className="page-link">
+                    2
+                  </a>
+                </li>
+                <li className="page-item active">
+                  <a href="#" className="page-link">
+                    3
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a href="#" className="page-link">
+                    4
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a href="#" className="page-link">
+                    5
+                  </a>
+                </li>
+                <li className="page-item">
+                  <a href="#" className="page-link">
+                    Next
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
